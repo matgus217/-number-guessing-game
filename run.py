3d20668c667f40a4d7481672eda4b26a40989c32
matgus217 <>
2024-01-29 06:05:40 +0000

    Add text in file run.py

--- run.py
+++ run.py
@@ -1,3 +1,65 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+
+
+def get_user_name():
+
+    while True:
+        print('Welcome to Number Guessing Game. Guess the number the computer has chosen')
+        print('You can only guess numbers, good luck!\n')
+        print("Enter your name:")
+        name = input()
+        print(f"Hello {name}! Let's play!\n")
+        break
+
+
+def user_guess(x):
+    """
+    Function that generates a random number between 1 and 25
+    """
+    while True:
+        try:
+            number = random.randint(1, x)
+            user_guess = 0
+            while user_guess != number:
+                user_guess = int(input(f'Make a guess between 1 and {x}:\n'))
+
+                if (user_guess < number):
+                    print('Sorry you guessed too low, guess again!\n')
+                elif (user_guess > number):
+                    print('Sorry you guessed too high, guess again!\n')
+                else:
+                    print(f'Woho you guessed correct, it was {number}!\n')
+            break
+        except ValueError:
+            print('Only enter numbers please, try again:')
+
+
+def computer_guess(x):
+    """
+    Function that makes the computer guess a number the user think about
+    """
+
+    print(f"Now it's your turn to pick a number between 1 and {x}!\n")
+    print('The computer will now guess your number.')
+    print("Press H if it's to high, L if it's to low, R if right!\n")
+
+    low = 1
+    high = x
+    response = ''
+    while response != 'r':
+        guess = random.randint(low, high)
+        response = input(f'Is {guess} too low(L), too high(H), or right(R?)\n')
+        if response == 'l':
+            low = guess + 1
+        elif response == 'h':
+            high = guess - 1
+        else:
+            print(f'The computer guessed your number, {guess}!')
+
+
+"""
+Calling functions
+"""
+get_user_name()
+user_guess(25)
+computer_guess(25)
